014b46008cda59efcfe63e7cff86f2d848fba07f
lorenzodamora <damora.lorenzo.studente@itispaleocapa.it>
2023-10-03 17:08:40 +0200

    Aggiungere i file di progetto.

---
+++ diplomati/Diplomati.cs
@@ -0,0 +1,107 @@
+﻿using System;
+
+namespace es5__23_24
+{
+	public class Diplomati
+	{
+		private Diplomato[] elencoDiplomati;
+		private short diplomatiCounter;
+
+		public Diplomati(int tot = 20)
+		{
+			elencoDiplomati = new Diplomato[tot];
+			diplomatiCounter = 0;
+		}
+
+		public Diplomato this[int index]
+		{
+			get
+			{
+				if(index >= 0 && index < diplomatiCounter)
+					return elencoDiplomati[index];
+				throw new IndexOutOfRangeException("L'indice specificato non è valido.");
+			}
+			set
+			{
+				if(index >= 0 && index < diplomatiCounter)
+					elencoDiplomati[index] = value;
+				else
+					throw new IndexOutOfRangeException("L'indice specificato non è valido.");
+			}
+		}
+
+		public Diplomato[] GetElenco() { return elencoDiplomati; }
+
+		private static int GetLength(int tot)
+		{
+			if(tot < 20) return 20;
+			return tot/20 * 20 + 20;
+		}
+
+		private static void Resize(ref Diplomato[] array, int newSize)
+		{
+			if(array.Length != newSize)
+			{
+				Diplomato[] array2 = new Diplomato[newSize];
+				if(array.Length < newSize) //se newsize è più grande, copia fino ad array.length e il resto rimane default
+					for(int i = 0; i < array.Length; i++)
+						array2[i] = array[i];
+				else //se newsize è più piccolo copia fino a newsize
+					for(int i = 0; i < newSize; i++)
+						array2[i] = array[i];
+				array = array2;
+			}
+		}
+
+		public void AggiungiDiplomato(Diplomato diplomato)
+		{
+			if(diplomatiCounter < elencoDiplomati.Length)
+			{
+				elencoDiplomati[diplomatiCounter] = diplomato;
+				diplomatiCounter++;
+			}
+			else
+			{
+				//throw new InvalidOperationException("L'elenco dei voti è pieno.");
+				//resize
+				Resize(ref elencoDiplomati, GetLength(diplomatiCounter));
+				AggiungiDiplomato(diplomato);
+			}
+		}
+
+		public string[] GetNominativi()
+		{
+			string[] result = new string[diplomatiCounter];
+			for(int i = 0; i < diplomatiCounter; i++)
+				result[i] = elencoDiplomati[i].Nominativo;
+
+			return result;
+		}
+
+		public string[] GetAbiliAiConcorsi()
+		{
+			int c = 0;
+			for(int i = 0; i < diplomatiCounter; i++)
+				if(elencoDiplomati[i].GetAbileAiConcorsi())
+					c++;
+
+			string[] result = new string[c];
+			c = 0;
+
+			for(int i = 0; i < diplomatiCounter; i++)
+				if(elencoDiplomati[i].GetAbileAiConcorsi())
+					result[c++] = elencoDiplomati[i].Nominativo;
+
+			return result;
+		}
+
+		/// <summary>
+		/// ordine alfabetico
+		/// </summary>
+		public void OrdinaPerNominativi()
+		{
+			DiplomatiSort.Quick(elencoDiplomati, 0, diplomatiCounter - 1);
+		}
+
+	}
+}
---
+++ diplomati/Diplomato.cs
@@ -0,0 +1,120 @@
+﻿//using System;
+
+namespace es5__23_24
+{
+	public class Diplomato
+	{
+		private string _nominativo = "nessun nominativo";
+		private float _voto = 0f;
+		private bool _old = false; //se false da 60 a 100, se true da 36 a 60.
+
+		private int _minVoto = 60; //36
+		private int _maxVoto = 100; //60
+		private int _minConcorsi = 70; //42
+
+		/**
+		 * <summary>
+		 * Ottiene o imposta il nominativo (nome e cognome).
+		 * </summary>
+		 */
+		public string Nominativo
+		{
+			get { return _nominativo; }
+			set { SetNominativo(value); }
+		}
+
+		public float Voto
+		{
+			get { return _voto; }
+			set { SetVoto(value); }
+		}
+
+		public bool Old
+		{
+			get { return _old; }
+		}
+
+		//
+		public Diplomato(string nominativo = "nessun nominativo", float voto = 0f, bool vecchia_notazione = false)
+		{
+			if(!SetDiplomato(nominativo, voto))
+			{
+				_nominativo = "inserimento non valido, [throw]";
+				_voto = 0f;
+			}
+			else
+			{
+				_old = vecchia_notazione;
+				if(_old)
+				{
+					_minVoto = 36;
+					_maxVoto = 60;
+					_minConcorsi = 42;
+				}
+			}
+		}
+
+		public Diplomato(string nominativo, string voto_stringa, bool vecchia_notazione = false)
+		{
+			if(!SetDiplomato(nominativo, voto_stringa))
+			{
+				_nominativo = "inserimento non valido, [throw]";
+				_voto = 0f;
+			}
+			else
+			{
+				_old = vecchia_notazione;
+				if(_old)
+				{
+					_minVoto = 36;
+					_maxVoto = 60;
+					_minConcorsi = 42;
+				}
+			}
+		}
+
+		//
+		public bool SetNominativo(string nominativo)
+		{
+			if(string.IsNullOrEmpty(nominativo)) return false;
+			_nominativo = nominativo;
+			return true;
+		}
+
+		public bool SetVoto(float voto)
+		{
+			//(_old ? 36 : 60) (_old ? 60 : 100)
+			if(voto < _minVoto) return false; // non si è diplomato
+			if(voto > _maxVoto) return false; // è oltre i valori consentiti
+			_voto = voto;
+			return true;
+		}
+
+		public bool SetVoto(string voto_stringa)
+		{
+			if(float.TryParse(voto_stringa, out float voto) || voto < _minVoto || voto > _maxVoto)
+			{
+				_voto = voto;
+				return true;
+			}
+			else
+				return false;
+		}
+
+		public bool SetDiplomato(string nominativo, float voto)
+		{
+			return SetNominativo(nominativo) & SetVoto(voto);
+		}
+
+		public bool SetDiplomato(string nominativo, string voto_stringa)
+		{
+			return SetNominativo(nominativo) & SetVoto(voto_stringa);
+		}
+
+		//
+		public bool GetAbileAiConcorsi()
+		{
+			return _voto >= _minConcorsi;
+		}
+	}
+}
